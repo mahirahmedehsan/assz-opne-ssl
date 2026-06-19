9fe486b04cd8cca6a2964fdaff0ccab7b63ae7e9
Shimul <mahirahmedehsan.com>
2026-06-20 03:11:36 +0600

    first commit

--- server.js
+++ server.js
@@ -70,7 +70,11 @@ app.use('/api/users', userRoutes);
 app.use('/api/contact', contactRoutes);
 app.use('/api/hero-slides', heroSlideRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.VERCEL !== '1') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
